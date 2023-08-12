1a2be648e48e16266b910adf2ff0eb3318325a2e
leobuck <leobuck12@gmail.com>
2023-08-12 11:58:27 -0300

    Criando a lista no localStorage

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
@@ -7,10 +7,11 @@ import { Item } from 'src/app/interfaces/iItem';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit, OnChanges {
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() item!: Item;
   @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoIdParaDeletar = new EventEmitter();
 
   faPen = faPen;
   faTrash = faTrash
@@ -35,5 +36,14 @@ export class ItemComponent implements OnInit, OnChanges {
     } else {
       this.item.comprado = true;
     }
+  }
+
+  deletarItem() {
+    console.log('Estão tentando me calar.');
+    this.emitindoIdParaDeletar.emit(this.item.id);
+  }
+
+  ngOnDestroy(): void {
+    console.log('Conseguiram me calar.');
   }
 }
