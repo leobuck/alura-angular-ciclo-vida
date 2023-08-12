494735018250cf7ea2016ce55bbed1c466320d92
leobuck <leobuck12@gmail.com>
2023-08-11 22:32:30 -0300

    @Input(): comunicação de pai para filho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ import { ListaDeCompraService } from './service/lista-de-compra.service';
 })
 export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
-  listaDeCompra! : Array<Item>
+  listaDeCompra! : Array<Item>;
+  itemParaSerEditado! : Item;
 
   constructor(private listaService: ListaDeCompraService) { }
 
@@ -19,6 +20,6 @@ export class AppComponent implements OnInit {
   }
 
   editarItem(item: Item) {
-    console.log(item);
+    this.itemParaSerEditado = item;
   }
 }
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
 import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
@@ -6,10 +7,18 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+  @Input() itemQueVaiSerEditado!: Item;
+
   valorItem!: string;
 
   constructor(private listaService: ListaDeCompraService) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!changes['itemQueVaiSerEditado'].firstChange) {
+      this.valorItem = this.itemQueVaiSerEditado?.nome;
+    }
+  }
 
   ngOnInit(): void { }
 
